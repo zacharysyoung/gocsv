f2141dd2706cd3903fd24084a9a9913f2deccf6d
Zach Young <zacharysyoung@gmail.com>
2023-11-02 19:31:08 -0700

    added asCSV to describe

--- cmd/describe.go
+++ cmd/describe.go
@@ -3,9 +3,12 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"strconv"
 )
 
-type DescribeSubcommand struct{}
+type DescribeSubcommand struct {
+	asCsv bool
+}
 
 func (sub *DescribeSubcommand) Name() string {
 	return "describe"
@@ -17,27 +20,42 @@ func (sub *DescribeSubcommand) Description() string {
 	return "Get basic information about a CSV."
 }
 func (sub *DescribeSubcommand) SetFlags(fs *flag.FlagSet) {
+	fs.BoolVar(&sub.asCsv, "csv", false, "Output results as CSV")
 }
 
 func (sub *DescribeSubcommand) Run(args []string) {
 	inputCsvs := GetInputCsvsOrPanic(args, 1)
-	DescribeCsv(inputCsvs[0])
+	DescribeCsv(inputCsvs[0], sub.asCsv)
 }
 
-func DescribeCsv(inputCsv *InputCsv) {
+func DescribeCsv(inputCsv *InputCsv, asCsv bool) {
 	imc := NewInMemoryCsvFromInputCsv(inputCsv)
 
-	numRows := imc.NumRows()
 	numColumns := imc.NumColumns()
 
-	fmt.Println("Dimensions:")
-	fmt.Printf("  Rows: %d\n", numRows)
-	fmt.Printf("  Columns: %d\n", numColumns)
-	fmt.Println("Columns:")
-
+	var descriptions [][]string
 	for i := 0; i < numColumns; i++ {
-		columnType := imc.InferType(i)
-		fmt.Printf("  %d: %s\n", i+1, imc.header[i])
-		fmt.Printf("    Type: %s\n", ColumnTypeToString(columnType))
+		descriptions = append(descriptions, []string{
+			strconv.Itoa(i + 1),
+			imc.header[i],
+			ColumnTypeToString(imc.InferType(i)),
+		})
+	}
+
+	var outputCsv *OutputCsv
+	if asCsv {
+		outputCsv = NewOutputCsvFromInputCsv(inputCsv)
+		outputCsv.Write([]string{"Column", "Name", "Type"})
+	} else {
+		fmt.Println("Columns:")
+	}
+
+	for _, x := range descriptions {
+		if asCsv {
+			outputCsv.Write(x)
+		} else {
+			fmt.Printf("  %s: %s\n", x[0], x[1])
+			fmt.Printf("    Type: %s\n", x[2])
+		}
 	}
 }
