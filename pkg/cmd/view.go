c565d4593305597dcdd15cc7b86f8889ea05b619
Zach Young <zacharysyoung@gmail.com>
2024-03-25 21:08:59 -0700

    working

--- pkg/cmd/view.go
+++ pkg/cmd/view.go
@@ -24,53 +24,40 @@ func (View) Run(r io.Reader, w io.Writer, args ...string) error {
 	}
 
 	if *mdflag {
-		return printMarkdown(w, recs)
+		printMarkdown(w, recs)
 	} else {
-		return printSimple(w, recs)
+		printSimple(w, recs)
 	}
-}
-
-func printSimple(w io.Writer, recs [][]string) error {
-	widths := maxWidths(recs)
-	types := inferCols(recs[1:])
-
-	// header row
-	sep := ""
-	for i, x := range recs[0] {
-		_, err := fmt.Fprintf(w, "%s%s", sep, paddedCell(x, stringType, widths[i]))
-		if err != nil {
-			return err
-		}
-		sep = ", "
-	}
-	if _, err := fmt.Fprint(w, "\n"); err != nil {
-		return err
-	}
-
-	// body rows
-	for _, rec := range recs[1:] {
-		sep = ""
-		for i, x := range rec {
-			_, err := fmt.Fprintf(w, "%s%s", sep, paddedCell(x, types[i], widths[i]))
-			if err != nil {
-				return err
-			}
-			sep = ", "
-		}
-		if _, err := fmt.Fprint(w, "\n"); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
-func printMarkdown(w io.Writer, recs [][]string) error {
+func printSimple(w io.Writer, recs [][]string) {
+	widths := maxWidths(recs)
+	types := inferCols(recs[1:])
+
+	sep := ""
+	for i, x := range recs[0] {
+		fmt.Fprintf(w, "%s%s", sep, pad(x, stringType, widths[i]))
+		sep = ", "
+	}
+	fmt.Fprint(w, "\n")
+
+	for _, rec := range recs[1:] {
+		sep = ""
+		for i, x := range rec {
+			fmt.Fprintf(w, "%s%s", sep, pad(x, types[i], widths[i]))
+			sep = ", "
+		}
+		fmt.Fprint(w, "\n")
+	}
+}
+
+func printMarkdown(w io.Writer, recs [][]string) {
 	widths := maxWidths(recs)
 	types := inferCols(recs[1:])
 
 	for i, x := range recs[0] {
-		fmt.Fprintf(w, "| %s ", paddedCell(x, stringType, widths[i]))
+		fmt.Fprintf(w, "| %s ", pad(x, stringType, widths[i]))
 	}
 	fmt.Fprint(w, "|\n")
 
@@ -89,12 +76,10 @@ func printMarkdown(w io.Writer, recs [][]string) error {
 
 	for _, rec := range recs[1:] {
 		for i := range rec {
-			fmt.Fprintf(w, "| %s ", paddedCell(rec[i], types[i], widths[i]))
+			fmt.Fprintf(w, "| %s ", pad(rec[i], types[i], widths[i]))
 		}
 		fmt.Fprint(w, "|\n")
 	}
-
-	return nil
 }
 
 func maxWidths(recs [][]string) []int {
@@ -109,14 +94,11 @@ func maxWidths(recs [][]string) []int {
 	return widths
 }
 
-func paddedCell(x string, t inferredType, w int) string {
-	n := w - len(x)
-	pad := strings.Repeat(" ", n)
-	switch t {
-	case stringType:
-		return x + pad
-	default:
-		return pad + x
-
+// pad pads x with n-number spaces; left-justify strings,
+// right-justify all other inferredTypes.
+func pad(x string, t inferredType, n int) string {
+	if t == stringType {
+		n *= -1
 	}
+	return fmt.Sprintf("%*s", n, x)
 }
